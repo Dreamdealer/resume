0b6620bed6d0a7f2f72f6f93f2a6992cfc4a3cd0
Johan van Tongeren <johan@mijnwebwinkel.nl>
2021-08-18 11:47:49 +0200

    chore: Added empty highscore entries

--- src/Components/Highscores.tsx
+++ src/Components/Highscores.tsx
@@ -11,18 +11,33 @@ type PropsType = {
     highscores: HighscoreType[];
 };
 
+const emptyHighscoreEntry: HighscoreType = {
+    name: '-----',
+    score: 0,
+    date: '0',
+};
+
 const SingleTetromino: FC<PropsType> = props => {
+    const highscores = props.highscores
+        .sort((a, b) => (a.score > b.score ? -1 : b.score > a.score ? 1 : 0))
+        .slice(0, 3);
+
+    // add three items, to make sure there's always three entries showing in the list
+    const populatedHighscores: HighscoreType[] = [
+        ...highscores,
+        emptyHighscoreEntry,
+        emptyHighscoreEntry,
+        emptyHighscoreEntry,
+    ];
+
     return (
         <Box direction="column" grow={1} width="100%">
-            {props.highscores
-                .sort((a, b) => (a.score > b.score ? -1 : b.score > a.score ? 1 : 0))
-                .slice(0, 3)
-                .map((player: HighscoreType) => (
-                    <Box grow={1} justifyContent="space-between">
-                        <Box>{player.name.toUpperCase()}</Box>
-                        <Box>{player.score}</Box>
-                    </Box>
-                ))}
+            {populatedHighscores.slice(0, 3).map((player: HighscoreType) => (
+                <Box grow={1} justifyContent="space-between">
+                    <Box>{player.name.toUpperCase()}</Box>
+                    <Box>{player.score}</Box>
+                </Box>
+            ))}
         </Box>
     );
 };
